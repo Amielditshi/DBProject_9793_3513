7530d2113e594d743e458bb0b98d7c81cc1e350a
dandanseb <dansebbah2@gmail.com>
2025-05-19 18:05:01 +0300

    Update select_contracts_per_creator_year_.sql

--- Stage2/Queries/select_queries/select_contracts_per_creator_year_.sql
+++ Stage2/Queries/select_queries/select_contracts_per_creator_year_.sql
@@ -2,10 +2,10 @@ SELECT
     CC.Content_CreatorFullName,
     A.AgentFullName,
     EXTRACT(YEAR FROM C.StartDate) AS ContractYear,
-    COUNT(C.ContractID) AS NumContracts,
     SUM(C.Payment) AS TotalPayments
 FROM Contract C
 JOIN Content_Creator CC ON C.CreatorID = CC.CreatorID
 LEFT JOIN Agent A ON CC.AgentID = A.AgentID
+WHERE C.StartDate >= CURRENT_DATE - INTERVAL '5 years'
 GROUP BY CC.Content_CreatorFullName, A.AgentFullName, EXTRACT(YEAR FROM C.StartDate)
 ORDER BY TotalPayments DESC;
