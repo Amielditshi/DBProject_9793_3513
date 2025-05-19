c900ccfb7f1dea68e4722764489ba5af788a9f07
dandanseb <dansebbah2@gmail.com>
2025-05-19 16:59:27 +0300

    Update select_active_creators_per_agent.sql

--- Stage2/Queries/select_queries/select_active_creators_per_agent.sql
+++ Stage2/Queries/select_queries/select_active_creators_per_agent.sql
@@ -1,9 +1,11 @@
--- This query calculates the number of active content creators represented by each agent.
 SELECT
     A.AgentFullName,
-    COUNT(CC.CreatorID) AS ActiveCreators
+    COUNT(CC.CreatorID) AS ActiveCreators,
+    AVG(DATE_PART('year', CURRENT_DATE) - DATE_PART('year', CC.JoinDate)) AS Avg_Years_With_Agent
 FROM Agent A
 JOIN Content_Creator CC ON A.AgentID = CC.AgentID
 WHERE CC.IsActive = TRUE
 GROUP BY A.AgentFullName
-ORDER BY ActiveCreators DESC;
+HAVING COUNT(CC.CreatorID) >= 2
+   AND AVG(DATE_PART('year', CURRENT_DATE) - DATE_PART('year', CC.JoinDate)) >= 2
+ORDER BY Avg_Years_With_Agent DESC;
