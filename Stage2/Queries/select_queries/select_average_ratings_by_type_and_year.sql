40213137955c5d1747d09ba113744f3a3f6633a4
dandanseb <dansebbah2@gmail.com>
2025-05-21 18:10:59 +0300

    Delete Stage2/Queries/select_queries/select_average_ratings_by_type_and_year.sql

--- Stage2/Queries/select_queries/select_average_ratings_by_type_and_year.sql
+++
@@ -1,9 +0,0 @@
--- This query calculates the average production rating for each production type 
--- and the release year.
-SELECT
-    ProductionType,
-    EXTRACT(YEAR FROM ReleaseDate) AS ReleaseYear,
-    AVG(ProductionRating) AS AverageRating
-FROM Production
-GROUP BY ProductionType, EXTRACT(YEAR FROM ReleaseDate)
-ORDER BY AverageRating DESC;
