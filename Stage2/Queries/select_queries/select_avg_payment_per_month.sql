90a9f7863229665daf33d2f075a404546e84ff4b
dandanseb <dansebbah2@gmail.com>
2025-05-21 18:11:11 +0300

    Delete Stage2/Queries/select_queries/select_avg_payment_per_month.sql

--- Stage2/Queries/select_queries/select_avg_payment_per_month.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    CC.Content_CreatorFullName,
-    EXTRACT(YEAR FROM C.StartDate) AS StartYear,
-    EXTRACT(MONTH FROM C.StartDate) AS StartMonth,
-    ROUND(AVG(C.Payment), 2) AS AvgPayment
-FROM Contract C
-JOIN Content_Creator CC ON C.CreatorID = CC.CreatorID
-GROUP BY CC.Content_CreatorFullName, EXTRACT(YEAR FROM C.StartDate), EXTRACT(MONTH FROM C.StartDate)
-ORDER BY AvgPayment DESC;
