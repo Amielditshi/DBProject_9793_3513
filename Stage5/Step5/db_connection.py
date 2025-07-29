d940fe77320d23af7961fa2b1c6c86c9e95c9437
dandanseb <dansebbah2@gmail.com>
2025-07-29 12:36:53 +0300

    Update db_connection.py

--- Stage5/Step5/db_connection.py
+++ Stage5/Step5/db_connection.py
@@ -1,14 +1,23 @@
 import psycopg2
-
+# ------------------------------------------------------------
+# Function: connect_to_db
+# ------------------------------------------------------------
+# This function establishes a connection to the PostgreSQL database
+# using the provided host, port, database name, user, and password.
+# If the connection is successful, it returns a connection object
+# that can be used to interact with the database.
+# In case of an error (e.g., wrong credentials or server not reachable),
+# it prints an error message and returns None.
+# ------------------------------------------------------------
 
 def connect_to_db():
     try:
         conn = psycopg2.connect(
             host="localhost",
             port="5432",
-            dbname="lastdb",
-            user="dan",
-            password="rebeccawife"
+            dbname="______",
+            user="_______",
+            password="______"
         )
         return conn
     except psycopg2.Error as e:
